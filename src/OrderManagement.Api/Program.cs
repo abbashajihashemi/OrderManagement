5c45d3f497018251afa34e83beedda2a8c8b1372
abbas <abbas.hajihashemi@ymail.com>
2026-09-19 21:53:48 +0330

    Complete healthcheck and env API-PORT

--- src/OrderManagement.Api/Program.cs
+++ src/OrderManagement.Api/Program.cs
@@ -4,11 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHealthChecks();
 
 builder.Services.AddVersioningApis();
 
 var app = builder.Build();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
